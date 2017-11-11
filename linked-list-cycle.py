6fc3b2d3793430ae2e34a160943b9296cef457a4
Shreya Srivastava <shreya92srivastava@gmail.com>
2017-11-10 18:32:09 -0700

    Update linked-list-cycle.py

--- linked-list-cycle.py
+++ linked-list-cycle.py
@@ -1,6 +1,15 @@
-# 
 
-#         self.next = None
+# Time:  O(n)
+# Space: O(1)
+#
+# Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+# 
+# Follow up:
+# Can you solve it without using extra space?
+#
+
+# Definition for singly-linked list.
+
 class Node:
     def __init__(self, item):
         self.data=item
