9fd0d8ccaa9bec6c4c423e9620db6af19ccd35f8
Shreya Srivastava <shreya92srivastava@gmail.com>
2017-11-11 13:48:15 -0700

    Add files via upload

---
+++ Python/linked-list-cycle.py
@@ -0,0 +1,30 @@
+# 
+
+#         self.next = None
+class Node:
+    def __init__(self, item):
+        self.data=item
+        self.next=None
+
+class Solution(object):
+    def hasCycle(self, head):
+        slow=head
+        fast=head
+        while fast and fast.next:
+            slow=slow.next
+            fast=fast.next.next
+
+            if fast is slow:
+                return True
+        return False
+
+
+"""if __name__=="__main__":
+head=Node(1)
+head.next=Node(2)
+head.next.next=Node(3)
+head.next.next.next=Node(4)
+head.next.next.next.next=head.next
+print (Solution().hasCycle(head))
+
+"""
