288ac0b0c23dec2c6a80f0dc6a39242aa0afa0b5
Shreya Srivastava <shreya92srivastava@gmail.com>
2017-11-14 21:17:16 -0700

    Add files via upload

--- Python/two-sum.py
+++ Python/two-sum.py
@@ -12,3 +12,22 @@ return [0, 1
 
 
 """
+
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        if len(nums)<=1:
+            return False
+        
+        dict_sum={}
+        
+        for i in range(len(nums)):
+            if nums[i] in dict_sum:
+                return [dict_sum[nums[i]],i]
+            else:
+                dict_sum[target-nums[i]]=i
